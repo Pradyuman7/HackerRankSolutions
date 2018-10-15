8f1a9cbc42e74ca121eb826333462f7bd6c98ba7
Pradyuman Dixit <41565823+Pradyuman7@users.noreply.github.com>
2018-10-15 10:06:47 +0200

    Create CaesarCipher.java

---
+++ CaesarCipher.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the caesarCipher function below.
+    static String caesarCipher(String talk, int k) {
+         k = k % 26 + 26;
+        StringBuilder encoded = new StringBuilder();
+        for (char i : talk.toCharArray()) {
+            if (Character.isLetter(i)) {
+                if (Character.isUpperCase(i)) {
+                    encoded.append((char) ('A' + (i - 'A' + k) % 26 ));
+                }
+                else {
+                    encoded.append((char) ('a' + (i - 'a' + k) % 26 ));
+                }
+            }
+            else {
+                encoded.append(i);
+            }
+        }
+        return encoded.toString();
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int k = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String result = caesarCipher(s, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
